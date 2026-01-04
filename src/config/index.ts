18f11545f153a49ead4746a1a96be2400376ff0b
Sanin <sanin9841393780@gmail.com>
2026-01-04 20:33:50 +0545

    index

---
+++ src/config/index.ts
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+
+export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 3000; 
+export const MONGODB_URI: string = process.env.MONGODB_URI || "mongodb://localhost:27017/local-my-events-db";
+export const JWT_SECRET:string = process.env.JWT_SECRET || "secret_key";
