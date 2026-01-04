2cb93bcf3ec6bf33e45c72906429dc7aef48fc6a
Sanin <sanin9841393780@gmail.com>
2026-01-04 20:34:46 +0545

    created user route and authentication

---
+++ src/service/user.service.ts
@@ -0,0 +1,54 @@
+import { UserRepository } from "../repository/user.repository";
+import { CreateUserDTO, LoginUserDTO } from "../dtos/user.dto";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config";
+import { HttpError } from "../error/http-error";
+
+let userRepository = new UserRepository();
+
+export class UserService{
+  async createUser(data:CreateUserDTO) {
+    const emailCheck = await userRepository.getUserByEmail(data.email);
+
+    if(emailCheck) {
+      throw new HttpError(403,"Email already in use"); 
+    }
+
+    const hashedPassword = await bcryptjs.hash(data.password, 10);
+    data.password = hashedPassword;
+
+    const usernameCheck = await userRepository.getUserByUsername(data.username);
+
+    if(usernameCheck){
+      throw new HttpError(403,"Username already in use")
+    }
+
+    const newUser = await userRepository.createUser(data);
+
+    return newUser;
+  }
+
+  async loginUser(data: LoginUserDTO){
+    const user = await userRepository.getUserByEmail(data.email);
+    if(!user) {
+      throw new HttpError(404,"No user found")
+    }
+
+    const validPassword = await bcryptjs.compare(data.password, user.password);
+    if(!validPassword){
+      throw new HttpError(401,"Invalid Credentials")
+    }
+
+    const payload = {
+      id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email,
+      role: user.role
+    }
+
+    const token = jwt.sign(payload, JWT_SECRET, {expiresIn: "30d"});
+    return {token, user};
+  }
+}
