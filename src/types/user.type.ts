a0586521fbc455c725e74593722b8dd930cb0cb2
Sanin <sanin9841393780@gmail.com>
2026-01-16 13:16:17 +0545

    update dtos, model, repository,servide and type for user

--- src/types/user.type.ts
+++ src/types/user.type.ts
@@ -1,7 +1,6 @@
 import z from "zod";
 
 export const UserSchema = z.object({
-  username: z.string().min(3).max(30),
   email: z.email(),
   password: z.string().min(8),
   firstName : z.string().optional(),
