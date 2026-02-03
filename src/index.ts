0c6c9da0a43752906721483ca2f1b8a3f1f267a1
Sanin <sanin9841393780@gmail.com>
2026-02-03 16:08:11 +0545

    updated

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import authRouter from "./routes/auth.route";
 import adminRouter from "./routes/admin/user.route";
 import { connectDatabase } from "./database/mongodb";
 import path from "path";
+import morgan = require("morgan");
 
 dotenv.config()
 const app : Application= express();
@@ -17,6 +18,7 @@ let corsOptions = {
   origin: ["http://localhost:3000", "http://localhost:3005"] 
 }
 app.use(cors(corsOptions));
+app.use(morgan("dev"));
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 app.use(bodyParser.json());
 
